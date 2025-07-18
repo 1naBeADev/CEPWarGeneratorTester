9cb79c89807bd372b0b6a4908857ad6f0d591e3c
Kinnard Cote <100028131+1naBeADev@users.noreply.github.com>
2025-07-18 15:59:14 +0800

    Update generalFunctions.js

--- generalFunctions.js
+++ generalFunctions.js
@@ -1,3 +1,14 @@
+const concerns = [
+            " ", "CANNOT BROWSE", "CANNOT MAKE_RECEIVE CALL",
+            "DEVICE PROBLEM", "LAN IP CONFIGURATION", 
+            "LOW BANDWIDTH", "NO DIALTONE",
+            "NO INTERNET DATA CONNECTION", "POOR CALL QUALITY", "ROUTER CONFIGURATION", 
+            "SELECTIVE BROWSING", "SLOW_INTERMITTENT CONNECTION", 
+            "SPECIAL FEATURE CONFIGURATION", "VOICE AND DATA PROBLEM", 
+            "WLAN CONFIGURATION", "EMAIL PROBLEM WEBSITE IP LISTING", "Not Applicable",
+            "RECONNECTION FROM TD","RECONNECTION FROM OP"
+        ];
+
 document.addEventListener("DOMContentLoaded", function () {
     const concernFields = document.querySelectorAll("input[id^='concernSearch']");
     
@@ -7,15 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const dropdown = document.getElementById(`concernDropdown${suffix}`);
         const list = document.getElementById(`concernList${suffix}`);
 
-        const concerns = [
-            " ", "CANNOT BROWSE", "CANNOT MAKE_RECEIVE CALL",
-            "DEVICE PROBLEM", "LAN IP CONFIGURATION", 
-            "LOW BANDWIDTH", "NO DIALTONE",
-            "NO INTERNET DATA CONNECTION", "POOR CALL QUALITY", "ROUTER CONFIGURATION", 
-            "SELECTIVE BROWSING", "SLOW_INTERMITTENT CONNECTION", 
-            "SPECIAL FEATURE CONFIGURATION", "VOICE AND DATA PROBLEM", 
-            "WLAN CONFIGURATION", "EMAIL PROBLEM WEBSITE IP LISTING", "Not Applicable"
-        ];
+        
 
         // Populate list
         function populateConcerns() {
@@ -26,6 +29,8 @@ document.addEventListener("DOMContentLoaded", function () {
                 li.addEventListener("click", () => {
                     input.value = concern;
                     dropdown.classList.add("hideCH");
+
+                    input.dispatchEvent(new Event("input"));
                 });
                 list.appendChild(li);
             });
@@ -69,6 +74,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
+
 // reset script
 document.addEventListener("DOMContentLoaded", function () {
     const resetButtons = document.querySelectorAll("input[type='reset']");
@@ -76,6 +82,16 @@ document.addEventListener("DOMContentLoaded", function () {
     resetButtons.forEach(button => {
         button.addEventListener("click", function (event) {
             event.preventDefault(); 
+
+            const checkdocu = confirm("Have you notated in all applicable tools?");
+            if (!checkdocu){
+                return;
+            }
+
+            const checktag = confirm("Have you already tagged the interaction?")
+            if (!checktag){
+                return;
+            }
 
             const userConfirmed = confirm("Are you sure you want to reset the form?");
 
@@ -86,3 +102,96 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+
+// Instruction Div
+
+const formInput = document.querySelectorAll(".formInput")
+const instructionDiv =  document.querySelectorAll(".instructionDiv");
+const insField =  document.querySelectorAll(".insField");
+
+formInput.forEach(input =>{
+    input.addEventListener("input",()=>{
+        const value = input.value.trim().toUpperCase();
+        let match = false
+
+
+        for (let i = 0; i < concerns.length; i++){
+            if (value == concerns[i].toUpperCase()){
+                match = true;
+                break;
+            }
+        }
+
+        instructionDiv.forEach(instDiv => {
+            if(match == true){
+                instDiv.style.display = "flex";
+            }else{
+                instDiv.style.display = "none";
+            }
+        });
+
+        switch(value){
+            case "VOICE AND DATA PROBLEM":
+
+                insField.forEach(outputDiv=>{
+                    outputDiv.innerHTML = "";
+                    
+                    const ulnode = document.createElement("UL")
+
+                    const linode = document.createElement("li");
+                    const tnode = document.createTextNode("Please fill out all required fields.")
+                    linode.appendChild(tnode);
+
+
+                    const linode3 = document.createElement("li");
+                    const tnode3 = document.createTextNode("Ensure that the information is accurate.")
+                    linode3.appendChild(tnode3);
+
+                    const linode4 = document.createElement("li");
+                    const tnode4 = document.createTextNode("Please review your inputs before generating the notes.")
+                    linode4.appendChild(tnode4);
+
+                    const linode5 = document.createElement("li");
+                    const tnode5 = document.createTextNode("Please make sure that notations are notated in all applicable tools and have already tagged the transaction in ESA correctly.")
+                    linode5.appendChild(tnode5);
+
+                    const anode = document.createElement("a");
+                    anode.href = "https://pldt365.sharepoint.com/:x:/r/sites/LIT365/files/_layouts/15/Doc.aspx?sourcedoc=%7B401A7E1A-F254-4C00-98BA-A25E32154D60%7D&file=PLDT_ENTERPRISE_BSMP_CV_MATRIX.xlsx&action=default&mobileredirect=true"
+                    anode.textContent = "BSMP Matrix"
+                    anode.target = "_blank";
+                    const linode6 = document.createElement("li");
+                    const tnode61stPart = document.createTextNode("See ")
+                    const tnode62ndPart = document.createTextNode(" for CV line reading instructions and troubleshoot")
+                    linode6.appendChild(tnode61stPart);
+                    linode6.appendChild(anode);
+                    linode6.appendChild(tnode62ndPart);
+
+                     const anode2 = document.createElement("a");
+                    anode2.href = "https://pldt365.sharepoint.com/sites/LIT365/PLDT-Enterprise/Pages/EMAIL_CENTER/EMAIL_CENTER_HOTLINE_ENTERPRISE_REPAIR_SIMPLE_SERVICE_GENUINE.aspx"
+                    anode2.textContent = "Work Instruction Reference"
+                    anode2.target = "_blank";
+                    const linode7 = document.createElement("li");
+                    const tnode71stPart = document.createTextNode("See ")
+                    const tnode72ndPart = document.createTextNode(" for the WI reference for Voice and Data Concern")
+                    linode7.appendChild(tnode71stPart);
+                    linode7.appendChild(anode2);
+                    linode7.appendChild(tnode72ndPart);
+
+
+                    
+                    ulnode.appendChild(linode)
+                    ulnode.appendChild(linode3)
+                    ulnode.appendChild(linode4)
+                    ulnode.appendChild(linode5)
+                    ulnode.appendChild(linode6)
+                    ulnode.appendChild(linode7)
+
+                    outputDiv.appendChild(ulnode)
+                })
+            break;
+        }
+
+    })
+})
+
